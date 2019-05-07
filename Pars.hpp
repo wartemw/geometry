258df8b353d47f2c79386483ac9953e12a416162
igorkru1337 <krupenni@list.ru>
2019-05-07 21:15:02 +0700

    Add function Pars-string

---
+++ Pars.hpp
@@ -0,0 +1,14 @@
+#ifndef H_PARS
+#define H_PARS
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+extern int i;
+using namespace std;
+
+void skipspace(string a);
+string partStr(string a);
+int partStr2(string a);
+
+#endif
