535a696368a87277c9823ce5f501623c016b4ed3
igorkru1337 <krupenni@list.ru>
2019-05-07 21:25:30 +0700

    Add function output figures

---
+++ Output.cpp
@@ -0,0 +1,10 @@
+#include "Output.hpp"
+
+void outputF(figure *circle, int k)
+{
+	for(int j=0; j<k; j++)
+	{
+		circle[j].output();
+		cout<<"\tPerimetr = "<<2*Pi*circle[j].r<<endl<<"\tArea = "<<Pi*circle[j].r*circle[j].r<<endl;
+	}
+}
---
+++ Output.hpp
@@ -0,0 +1,13 @@
+#ifndef H_OUTPUT
+#define H_OUTPUT
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include"Figure.cpp"
+
+using namespace std;
+
+void outputF(figure *circle, int k);
+
+#endif
